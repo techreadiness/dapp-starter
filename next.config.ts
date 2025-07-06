77e170c43c8461578a7780e80e7be9e998b71b9e
ChipmunkForLove <52595663+ChipmunkForLove@users.noreply.github.com>
2025-07-06 23:11:37 +0900

    Add webpack alias for next.config.js (#2)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...(config.resolve.alias || {}),
+            '@/components': path.resolve(__dirname, 'components'),
+            '@/utils:': path.resolve(__dirname, 'utils'),
+            // 필요 시 더 추가
+        };
+        return config;
+    },
 };
 
 export default nextConfig;
