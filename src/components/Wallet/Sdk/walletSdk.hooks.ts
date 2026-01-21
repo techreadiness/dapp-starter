d8a7fc43f697b9b58edd825e0d2097dc6293b4fa
techreadiness <shared.ts.next@gmail.com>
2026-01-21 19:22:19 +0900

    Fix Transaction type

--- src/components/Wallet/Sdk/walletSdk.hooks.ts
+++ src/components/Wallet/Sdk/walletSdk.hooks.ts
@@ -51,8 +51,12 @@ export const useKaiaWalletSecurity = () => {
 export type Transaction = {
     from: string;
     to: string;
-    value: string;
-    gas: string;
+    value?: string;
+    gas?: string;
+    depositTokenAddress?:string;
+    depositAmount?:string;
+    input?: string;
+    typeInt?: number;
 }
 
 export const useKaiaWalletSdk = () => {
@@ -94,7 +98,7 @@ export const useKaiaWalletSdk = () => {
     },[walletProvider]);
 
     const sendTransaction = useCallback(async(params: Transaction[])=>{
-        await walletProvider.request({ method: 'kaia_sendTransaction', params: params });
+        return await walletProvider.request({ method: 'kaia_sendTransaction', params: params });
     },[walletProvider]);
 
     //
@@ -105,5 +109,6 @@ export const useKaiaWalletSdk = () => {
     const getErc20TokenBalance = useCallback(async(contractAddress:string,account:string)=>{
         return await walletProvider.getErc20TokenBalance(contractAddress,account);
     },[walletProvider]);
+
   return {getAccount, requestAccount, connectAndSign, disconnectWallet, getBalance, sendTransaction, getErc20TokenBalanceWithDepositedBalance, getErc20TokenBalance}
 };
